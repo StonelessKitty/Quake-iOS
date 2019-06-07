64ad3a56bedb6509cff4eea43ec7efb242445748
Elise Gibson-Baker <elise.sakura@gmail.com>
2019-06-07 19:07:23 +1200

    started adding legacy ``gamepad'' device support

--- Quake-iOS/GameControllerSetup.swift
+++ Quake-iOS/GameControllerSetup.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Heriberto Delgado on 7/8/16.
 //
+//  Edited by Elise Gibson-Baker on 06/06/19.
+//
 //
 
 import GameController
@@ -11,6 +13,10 @@ import GameController
 public var remote: GCController? = nil
 
 public var currentViewController: UIViewController? = nil
+
+public var strafe = false
+
+public var alwaysRun = false
 
 class GameControllerSetup: NSObject
 {
@@ -57,13 +63,13 @@ class GameControllerSetup: NSObject
                 
                 remote!.extendedGamepad!.buttonA.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
                     
-                        Sys_Key_Event(32, qboolean(pressed ? 1 : 0)) // K_ENTER, true / false
-
+                    Sys_Key_Event(32, qboolean(pressed ? 1 : 0)) // K_ENTER, true / false
+                    
                 }
                 
                 remote!.extendedGamepad!.buttonB.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
                     
-//                    Sys_Key_Event(27, qboolean(pressed ? 1 : 0)) // K_ESCAPE, true / false
+                    //                    Sys_Key_Event(27, qboolean(pressed ? 1 : 0)) // K_ESCAPE, true / false
                     
                 }
                 
@@ -111,10 +117,98 @@ class GameControllerSetup: NSObject
                 }
                 
                 break
+            } else if controller.gamepad != nil && remote == nil
+            {
+                remote = controller
+                
+                remote!.playerIndex = .index1
+                
+                remote!.controllerPausedHandler = { (controller: GCController) -> () in
+                    
+                    Sys_Key_Event(27, qboolean(1)) // K_ESCAPE, true
+                    Sys_Key_Event(27, qboolean(0)) // K_ESCAPE, false
+                    
+                }
+                
+                remote!.gamepad!.buttonA.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    Sys_Key_Event(32, qboolean(pressed ? 1 : 0))
+                    print("Pressed A")
+                    
+                }
+                
+                remote!.gamepad!.buttonB.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    //Sys_Key_Event(132, qboolean(pressed ? 1 : 0))
+                    if pressed && alwaysRun == false
+                    {
+                        print("B run", alwaysRun)
+                        Sys_Cbuf_AddText("+speed\n")
+                        Cbuf_Execute()
+                        alwaysRun = true
+                    } else if pressed && alwaysRun == true
+                    {
+                        print("B run", alwaysRun)
+                        Sys_Cbuf_AddText("-speed\n")
+                        Cbuf_Execute()
+                        alwaysRun = false
+                    }
+                    print("Pressed B")
+                }
+                
+                remote!.gamepad!.buttonY.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    Sys_Key_Event(47, qboolean(pressed ? 1 : 0))
+                    print("Pressed Y")
+                }
+                
+                remote!.gamepad!.dpad.up.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    
+                    Sys_Key_Event(128, qboolean(pressed ? 1 : 0)) // K_UPARROW, true / false
+                    print("Pressed DPad_Up")
+                }
+                
+                
+                remote!.gamepad!.dpad.left.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    
+                    Sys_Key_Event(130, qboolean(pressed ? 1 : 0)) // K_LEFTARROW, true / false
+                    print("Pressed DPad_Left")
+                }
+                
+                remote!.gamepad!.dpad.right.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    
+                    Sys_Key_Event(131, qboolean(pressed ? 1 : 0)) // K_RIGHTARROW, true / false
+                    print("Pressed DPad_Right")
+                }
+                
+                remote!.gamepad!.dpad.down.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    
+                    Sys_Key_Event(129, qboolean(pressed ? 1 : 0)) // K_DOWNARROW, true / false
+                    print("Pressed DPad_Down")
+                }
+                
+                remote!.gamepad!.rightShoulder.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    Sys_Key_Event(133, qboolean(pressed ? 1 : 0))
+                    print("Pressed Right_Shoulder")
+                }
+                
+                remote!.gamepad!.leftShoulder.pressedChangedHandler = { (button: GCControllerButtonInput, value: Float, pressed: Bool) -> () in
+                    if pressed && strafe == false
+                    {
+                        print("leftshoulder strafe", strafe)
+                        Sys_Cbuf_AddText("+strafe\n")
+                        Cbuf_Execute()
+                        strafe = true
+                    } else if pressed && strafe == true
+                    {
+                        print("leftshoulder strafe", strafe)
+                        Sys_Cbuf_AddText("-strafe\n")
+                        Cbuf_Execute()
+                        strafe = false
+                    }
+                }
             }
+            break
         }
     }
-
+    
     static func disconnect(_ controller: GCController?)
     {
         if remote == controller
